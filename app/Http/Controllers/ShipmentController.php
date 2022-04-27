556ce8470d35ae778ccbff65a2108602bc3334c8
Abdullah Hamdy <Obaida.hamdy.1998@gmail.com>
2022-04-27 17:50:38 +0200

    shipments model & controller

---
+++ app/Http/Controllers/ShipmentController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shipment;
+use Illuminate\Http\Request;
+
+class ShipmentController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Shipment $shipment)
+    {
+        //
+    }
+
+    public function edit(Shipment $shipment)
+    {
+        //
+    }
+
+    public function update(Request $request, Shipment $shipment)
+    {
+        //
+    }
+
+    public function destroy(Shipment $shipment)
+    {
+        //
+    }
+}
