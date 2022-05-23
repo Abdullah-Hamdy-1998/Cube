9d9e80dc590d28af9d935a286fc30e207dcea546
aya-kamal1 <sci.ayakamal71222@alexu.edu.eg>
2022-05-23 05:53:29 +0200

    one card only

--- resources/views/pages/inventory/index.blade.php
+++ resources/views/pages/inventory/index.blade.php
@@ -12,33 +12,6 @@
     <div class="mb-4"> <img src="{{ asset('images/tomato.WEBP') }}" class="d-inline mr-5"style="max-height:30px; height:auto;" ><p class="d-inline ">tomato</p></div>
     <div> <p class="d-inline p-coust ">weight</p> <p class="d-inline ml-5 p-coust">74 kg</p></div>
     <div> <p class="d-inline p-coust ">Quantity</p> <p class="d-inline ml-5 p-coust">32 u</p></div></div></div>
-    
-
-    <div class="card pl-5 mr-4 col-3 d-inline-block"style="max-width:300px; min-width:300px;">
-        <div class="card-body ">
-    <div class="mb-4"> <img src="{{ asset('images/tomato.WEBP') }}" class="d-inline mr-5"style="max-height:30px; height:auto;" ><p class="d-inline ">tomato</p></div>
-    <div> <p class="d-inline p-coust ">weight</p> <p class="d-inline ml-5 p-coust">74 kg</p></div>
-    <div> <p class="d-inline p-coust ">Quantity</p> <p class="d-inline ml-5 p-coust">32 u</p></div></div></div>
-    <div class="card pl-5 mr-4 col-3 d-inline-block"style="max-width:300px; min-width:300px;">
-        <div class="card-body ">
-    <div class="mb-4"> <img src="{{ asset('images/tomato.WEBP') }}" class="d-inline mr-5"style="max-height:30px; height:auto;" ><p class="d-inline ">tomato</p></div>
-    <div> <p class="d-inline p-coust ">weight</p> <p class="d-inline ml-5 p-coust">74 kg</p></div>
-    <div> <p class="d-inline p-coust ">Quantity</p> <p class="d-inline ml-5 p-coust">32 u</p></div></div></div>
-    <div class="card pl-5 mr-4 col-3 d-inline-block"style="max-width:300px; min-width:300px;">
-        <div class="card-body ">
-    <div class="mb-4"> <img src="{{ asset('images/tomato.WEBP') }}" class="d-inline mr-5"style="max-height:30px; height:auto;" ><p class="d-inline ">tomato</p></div>
-    <div> <p class="d-inline p-coust ">weight</p> <p class="d-inline ml-5 p-coust">74 kg</p></div>
-    <div> <p class="d-inline p-coust ">Quantity</p> <p class="d-inline ml-5 p-coust">32 u</p></div></div></div>
-    <div class="card pl-5 mr-4 col-3 d-inline-block"style="max-width:300px; min-width:300px;">
-        <div class="card-body ">
-    <div class="mb-4"> <img src="{{ asset('images/tomato.WEBP') }}" class="d-inline mr-5"style="max-height:30px; height:auto;" ><p class="d-inline ">tomato</p></div>
-    <div> <p class="d-inline p-coust ">weight</p> <p class="d-inline ml-5 p-coust">74 kg</p></div>
-    <div> <p class="d-inline p-coust ">Quantity</p> <p class="d-inline ml-5 p-coust">32 u</p></div></div></div>
-    <div class="card pl-5 mr-4 col-3 d-inline-block"style="max-width:300px; min-width:300px;">
-        <div class="card-body ">
-    <div class="mb-4"> <img src="{{ asset('images/tomato.WEBP') }}" class="d-inline mr-5"style="max-height:30px; height:auto;" ><p class="d-inline ">tomato</p></div>
-    <div> <p class="d-inline p-coust ">weight</p> <p class="d-inline ml-5 p-coust">74 kg</p></div>
-    <div> <p class="d-inline p-coust ">Quantity</p> <p class="d-inline ml-5 p-coust">32 u</p></div></div></div>
 
 
     @endsection
