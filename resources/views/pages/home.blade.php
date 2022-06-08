c4f77607002d87ac04975f778472d63f1ef43b30
aya-kamal1 <sci.ayakamal71222@alexu.edu.eg>
2022-06-08 14:38:58 +0200

    home tables

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,40 +10,44 @@
 </div>
 <div class="card border-radius-coust" >
     <div class="card-body table-margin-coust">
-        <a><p class="float-right">view all</p></a>
-        <table id="datatable-buttons" class="table  mt-3 dt-responsive nowrap" style=" width: 100%; background-color: white;">
-        <thead class="">
-        <tr >
-        <th></th>
-        <th>iD</th>
-        <th>shipment type</th>
-        <th>cus.-sup.</th>
-        <th>description</th>
-        <th>modified</th>
-        </tr>
-        </thead>
-    
+        <a ><p class="float-right text_anchor"  >View All</p></a>
+        <table id="datatable-buttons"onload="setId();" class="table table-coust mt-3 text-center dt-responsive nowrap " style=" width: 100%; ">
+            <h5 class="m-0 mb-4">Leatest Shipments</h5>
+            <thead class="thead-coust">
+            <tr  >
+            <th  class="th-table-coust  ">ID<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+            <th  class="th-table-coust"> Shipment Type<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+            <th  class="th-table-coust">Cus.-Sup.<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+            <th  class="th-table-coust">Description<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+            <th  class="th-table-coust">Modified<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+            </tr>
+            </thead>
+            
         <tbody>
         </tbody>
     </table></div></div>
 
+
+
+    <div class="card border-radius-coust" >
+        <div class="card-body table-margin-coust">
+            <a ><p class="float-right text_anchor"  >View All</p></a>
+            <h5 class="m-0 mb-4">Recent Activities</h5>
+            <table id="datatable-buttons"onload="setId();" class="table table-coust mt-3 text-center dt-responsive nowrap " style=" width: 100%; ">
+                <thead class="thead-coust">
+                <tr  >
+                <th  class="th-table-coust  ">ID<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                <th  class="th-table-coust"> Shipment Type<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                <th  class="th-table-coust">Cus.-Sup.<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                <th  class="th-table-coust">Description<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                <th  class="th-table-coust">Modified<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                </tr>
+                </thead>
+                
+            <tbody>
+            </tbody>
+        </table></div></div>
+    
 @endsection
 @section('plugins')
-<script src="{{ asset('plugins/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
-
-<!-- Required datatable js -->
-<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
-<!-- Buttons examples -->
-<script src="{{ asset('plugins/datatables/jszip.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables/pdfmake.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables/vfs_fonts.js') }}"></script>
-<script src="{{ asset('plugins/datatables/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables/buttons.colVis.min.js') }}"></script>
-<!-- Responsive examples -->
-<script src="{{ asset('plugins/datatables/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
-
-<!-- Datatable init js -->
-<script src="{{ asset('pages/datatables.init.js') }}"></script>
 @endsection 
