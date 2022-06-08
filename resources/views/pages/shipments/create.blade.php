2871654375d91eccb21f57a4b7876a242cb30740
aya-kamal1 <sci.ayakamal71222@alexu.edu.eg>
2022-06-08 18:00:45 +0200

    add shipment item section

--- resources/views/pages/shipments/create.blade.php
+++ resources/views/pages/shipments/create.blade.php
@@ -139,7 +139,9 @@
                     @foreach ($suppliers as $supplier)
                         <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
                     @endforeach
-                    </optgroup>
+                    <option>aya</option>
+                </optgroup>
+
                 </select>
                 <label >coustmer/supplier</label>
             </div></div>
@@ -151,20 +153,20 @@
         </form>
     </div>
     </div>
-    <div class="card" style="border-radius:10px;">
+    <div class="card border-radius-coust">
         <div class="card-body pt-0 shadow-sm">
             <label class="label-coust pt-4 pb-4 " style="font-size: 19px; ">Shipment Items</label>
             <form>
-                <table class="table table-coust" id="table" style=" border-top-style: hidden">
+                <table class="table  text-center bitable-bordered dt-responsive nowrap" id="table" >
                     <thead>
                         <tr>
-
-                            <th class="pl-5 " scope="col">ID</th>
-                            <th scope="col">Avatar</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Weight</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Modified</th>
+                            
+                            <th class=" text-center " scope="col">ID<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                            <th scope="col">Avatar<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                            <th scope="col">Name<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                            <th scope="col">Weight<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                            <th scope="col">Quantity<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
+                            <th scope="col">Modified<i class="fas fa-sort-alpha-down ml-2  mt-1"></i></th>
 
                         </tr>
                     </thead>
@@ -173,47 +175,48 @@
                 </table>
             </form>
         </div>
-    </div>
-    <div class="row align-items-start">
+    </div>    <div class="row align-items-start">
         <div class="d-inline-block col-6  m-0">
             <div class="card   shadow-sm p-3 pl-4 pt-4" style="border-radius:10px;">
                 <label class="pb-1 label-coust" style="font-size: 19px;">Add shipment item</label>
                 <form class=" text-capitalize" onchange="validate();" action="" method="post" id="submitform">
                     <div class="pl-0 col-12   ">
                         <div class="div-coust">
-                            <label class="placeholder-coustt " style="left:12px;">shipment item</label>
                         </div>
-                        <select id="submitagain" class="form-control select2 mb-4  " style="border-radius: 12px  ">
+                        <div class="selectField">
+                        <select id="submitagain" class="form-control  select2 mb-4  " style="border-radius: 12px  ">
                             <option value="" disabled selected> Select </option>
                             @foreach ($items as $item)
                                 <option value="{{ $item->id }}">{{ $item->name }}</option>
                             @endforeach
                         </select>
-                    </div>
+                        <label  >shipment item</label>
+                    </div></div>
 
-                    <div class="div-coust pl-0  pb-4 form-floating col-12">
-                        <input type="number" class="form-control" style="border-radius: 12px  " placeholder="weight"
+                    <div class="div-coust pl-0  mb-4 inputField col-12">
+                        <input type="number" class="form-control border-radius-coust"  required placeholder=" "
                             id="WSum">
+                            <label>Weight</label>
                     </div>
-                    <div class=" pl-0  pb-4 form-floating col-12">
-                        <input type="number" class="form-control" style="border-radius: 12px  " placeholder="Quantity"
+                    <div class=" pl-0  mb-4 inputField col-12">
+                        <input type="number" class="form-control border-radius-coust" required placeholder=" "
                             id="Quantity">
+                            <label>Quantity</label>
+
                     </div>
                     <button type="button" onclick="table_again();" id="submit" disabled=true
-                        style="border-radius: 12px ;box-shadow: none;"
-                        class="btn  col-2 dropdown-coust ml-1  mb-4">Add</button>
+                        class="btn  col-2 dropdown-coust  ml-1 border-radius-coust  mb-4 all-buttons-coust">Add</button>
                 </form>
             </div>
             <div class="col-6 pl-0">
-                <button onclick="json_obj()" type="submit" style="border-radius: 12px ;box-shadow: none;" id="submi_final"
-                    disabled=true class="btn  col-4 d-inline   ">Add</button>
-                <button type="button" style="border-radius: 12px ;box-shadow: none;"
-                    class="btn btn-danger  ml-2  col-4 d-inline   ">Cancel</button>
+                <button onclick="json_obj()" type="submit"  id="submi_final"
+                    disabled=true class="btn  col-4 d-inline border-radius-coust all-buttons-coust  ">Add</button>
+                <button type="button" class="btn btn-danger border-radius-coust  ml-2  col-4 d-inline   ">Cancel</button>
 
             </div>
         </div>
         <div class="d-inline-block col-6 ml-auto">
-            <div class="card shadow-sm p-3 pl-4 pt-4" style="border-radius:10px;">
+            <div class="card shadow-sm p-3 pl-4 pt-4 border-radius-coust">
                 <div>
                     <label class="label-coust pb-1 label-coust" style="font-size: 19px;">Weight Point</label>
                     <div class="form-check form-check-inline ml-3 float-right">
@@ -224,29 +227,28 @@
 
                 <div class=" f-flex pl-0 pb-4 form-floating col-12">
                     <input type="text" class="form-control" placeholder="Unknown" id="floatinginput" id=" connect"
-                        type="text" style="border-radius: 12px;"></input type="text"><button class="button-coust"
+                        type="text" ></input type="text"><button class="all-buttons-coust border-radius-coust"
                         style=" box-shadow: none;color:white;background-color:#6c757d;Top:1px;cursor: pointer"><i
                             class="fas fa-redo"></i></button>
                 </div>
                 <div class=" d-flex pl-0 pb-4 form-floating col-12 ">
-                    <input type="number" class="form-control" style="height: 70px; border-radius: 12px;"
-                        placeholder="Weight" id="myInput" id="AddedWeight" style="border-radius: 12px;"><button
-                        class="ml-0 button-coust " style="box-shadow: none;height: 70px;  ">Kg</button>
+                    <input type="number" class="form-control border-radius-coust" style="height: 70px;"
+                        placeholder="Weight" id="myInput" id="AddedWeight" ><button
+                        class="ml-0 button-coust border-radius-coust" style="box-shadow: none;height: 70px;  ">Kg</button>
                 </div>
-                <button onclick="Add_Sum();" type="button" style="border-radius: 12px ;box-shadow: none;"
-                    class="btn ml-auto  col-2 d-inline  ">Add</button>
-                <label class=" label-coust pt-4 pb-4 d-inline " style="font-size: 19px;">Cumulative Weights </label>
+                <label class=" label-coust all pt-4 pb-4 d-inline  " style="font-size: 19px;">Cumulative Weights 
+                    <button onclick="Add_Sum();" type="button" class="btn ml-auto float-right mr- border-radius-coust all-buttons-coust col-2 d-inline  ">Add</button>
+                </label>
                 <table class="table " id="fetch" style="border-top-style: hidden">
                     <thead style="background-color: #f3f3f3">
                         <tr>
-                            <th class=" pl-5" scope="col">No</th>
-                            <th scope="col">Weight</th>
-                            <th scope="col"></th>
+                            <th class=" text-center" scope="col">No</th>
+                            <th scope="col" class="text-center" >Weight</th>
                             <th scope="col"></th>
 
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="text-center">
                         <tr>
                         </tr>
 
